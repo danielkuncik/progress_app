9ea8ad2956682698bb1d647236e1e870c616858f
Daniel Kuncik <dkuncik@gmail.com>
2017-03-26 11:47:36 -0400

    add-user-model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/index'
+
+  get '/users/:id', to: 'users#show'
+
   root 'application#hello'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#User.create(name: 'kuncik', section: 'massachusetts')
+#User.create(name: 'hamilton', section: 'new york')
+#User.create(name: 'washington', section: 'virginia')
+#User.create(name: 'jefferson', section: 'virginia')
+#User.create(name: 'franklin', section: 'pennsylvania')
+#User.create(name: 'madison', section: 'virgina')
+#User.create(name: 'adams', section: 'massachusetts')
+
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib','seeds','formative_results.csv'))
+
+
+CSV.parse(csv_text, :headers => true) do |row|
+  User.create(name: row['username'], section: row['section'],
+              password: row['passcode'], password_confirmation: row['password'])
+end
+
+#CSV.foreach('formative_results.csv', :headers => true)  do |row|
+#  User.create(name: row['name'], section: row['section'])
+#end
