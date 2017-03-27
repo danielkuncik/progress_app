2970199e6bc891127ea843e728f937eafa06c230
Daniel Kuncik <dkuncik@gmail.com>
2017-03-26 21:43:01 -0400

    improve grade display page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
     @quizzes = Quiz.all
     @grades = @user.grades
     @total_score = 0
+    @grades.each do |grade|
+      @total_score += grade.value
+    end
+
+    @category = nil
+    if @total_score >= 28
+      @category = 'Advanced'
+    elsif @total_score >= 20
+      @category = 'Proficient'
+    elsif @total_score >= 12
+      @category = "Needs Improvement"
+    else
+      @category = "Warning"
+    end
   end
 
   def index
