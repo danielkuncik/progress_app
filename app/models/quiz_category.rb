8bd309469cc6b71d280e087563260a44a2e3de01
Daniel Kuncik <dkuncik@gmail.com>
2017-04-12 16:41:02 -0400

    Add tests to quiz category model

--- app/models/quiz_category.rb
+++ app/models/quiz_category.rb
@@ -1,4 +1,6 @@
 class QuizCategory < ApplicationRecord
   has_many :quizzes
 
+  validates :name, presence: true, length: {minimum: 5, maximum: 40}
+
 end
--- test/models/quiz_category_test.rb
+++ test/models/quiz_category_test.rb
@@ -4,4 +4,21 @@ class QuizCategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @category = QuizCategory.new(name: "Example Category")
+  end
+
+  test "name should be present (nonblank)" do
+    @category.name = " " * 6
+    assert_not @category.valid?
+  end
+
+  test "name should not be too long or too short"  do
+    @category.name = "a" * 50
+    assert_not @category.valid?
+    @category.name = "a" * 3
+    assert_not @category.valid?
+  end
+
 end
